62a3c4db05642ad5b3bc82c1abe7b0c97148e88f
CJ3016366 <CJ3016366@LAB01-PC01>
2023-04-19 12:56:59 -0300

    Adicionar arquivos de projeto.

---
+++ ProvaPR21Bim/Program.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaPR21Bim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int exercicio;
+
+            //Console.WriteLine("digite um exercicio");
+            
+
+            //Console.WriteLine("menu");
+             //Console.WriteLine("1- exercicio 1");
+            //Console.WriteLine("2- exercicio 2");
+           // Console.WriteLine("3- exercicio 3");
+            //Console.WriteLine("4- exercicio 4");
+            //Console.WriteLine("5- exercicio 5");
+            //Console.WriteLine("6- exercicio 6");
+            //Console.WriteLine("7- exercicio 7");
+            //exercicio = int.Parse(Console.ReadLine());
+
+
+
+
+            //case 1:
+
+            //int numero;
+
+            //Console.WriteLine("escreva um numero inteiro");
+            //numero = int.Parse(Console.ReadLine());
+
+            //if (numero > 0.0f)
+            //{
+            //    Console.WriteLine("o numero é" + numero);
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("o numero inteiro é" + numero);
+            //}
+
+            //Console.ReadKey();
+
+
+            // case 2:
+
+            // int numero;
+
+            //Console.WriteLine("escreva um numero ");
+            //numero = int.Parse(Console.ReadLine());
+
+            //if (numero % 3 == 0 && numero % 5 == 0 && numero % 7 == 0)
+            //{
+            // Console.WriteLine("é multiplo de 3,5,4 e 7"
+            //}
+
+            //if (numero % 3 == 0)
+            //{
+            //    Console.WriteLine("é divisil por 3");
+            // }
+
+            //else if (numero % 5 == 0) ;
+            //{
+            //Console.WriteLine("é divisvel 5");
+            // }
+
+            //else if (numero % 4 == 0) ;
+            //{
+            // Console.WriteLine("é divisvel 4");
+            //}
+
+            //else if (numero % 7 == 0) ;
+            //{
+            // Console.WriteLine("é divisvel 7");
+            //}
+
+            //else
+            //{
+            //Console.WriteLine(" impossibilidade de divisão")
+            //}
+
+            //Console.ReadKey();
+
+
+
+
+
+           //case 3:
+
+            //int n1, n2,n3;
+
+            //nsole.WriteLine("escreva o numero inteiro");
+            // = int.Parse(Console.ReadLine());
+            //nsole.WriteLine("escreva outro numero inteiro");
+            // = int.Parse(Console.ReadLine());
+            //nsole.WriteLine("escreva outro numero inteiro");
+            // = int.Parse(Console.ReadLine());
+
+
+            // (n1 > n2 > && n1 > n3 && n2 > n3) ;
+            
+               //Console.WriteLine("essa é a ordem :" + n1 + n2 + n3);
+            
+
+            //if n2 > n1 > && n1 > n3 && n3 > n2) 
+            //{
+               // Console.WriteLine("essa é a ordem :" + n2 + n1 + n3);
+
+            //}
+
+            //else if (n2 > n1 > && n1 > n3 && n3 > n2) 
+            //{
+                //Console.WriteLine("essa é a ordem :" + n2 + n1 + n3);
+
+            //}
+
+            //else if (n2 > n1 > && n2 > n3 && n3 > n1) 
+            //{
+                //Console.WriteLine("essa é a ordem :" + n1 + n3 + n2);
+
+            //}
+
+            //else if (n3 > n1 > && n3 > n2 && n1 > n2) 
+            //{
+                //Console.WriteLine("essa é a ordem :" + n3 + n1 + n2);
+
+            //}
+
+            //else if (n3 > n1 > && n3 > n2 && n2 > n3) 
+            //{
+                 // Console.WriteLine("essa é a ordem" + n1 + n2 + n3);
+                //Console.WriteLine(+n1);
+               // Console.WriteLine(+n2);
+              //Console.WriteLine(+n3);
+            //}
+
+            //Console.ReadKey();
+
+
+
+
+            //case 4:
+
+                //nsole.WriteLine("escreav um numero de 1 a 7");
+            //= int.Parse(Console.ReadLine());
+
+           //onsole.WriteLine("---menu---");
+            //t p, dias; 
+            //Console.WriteLine("1- domindo");
+            //Console.WriteLine("2- segunda");
+            //Console.WriteLine("3- terça");
+          //Console.WriteLine("4- quarta");
+            //Console.WriteLine("5- quinta");
+            //Console.WriteLine("6- sexta");
+            //Console.WriteLine("7- sabado");
+            //p = int.Parse(Console.ReadLine());
+
+            //switch (p)
+            //{
+                //case 1:
+                    //Console.WriteLine("materia desse dia é: geografia, portgues e matematica");
+                   // break;
+
+                //case 2:
+                  //  Console.WriteLine("materia desse dia é: artes, biologia e sociologia");
+                   // break;
+
+                //case 3:
+                   // Console.WriteLine("materia desse dia é: filosofia, ingles e historia");
+                   // break;
+
+
+               // case 4:
+                   // Console.WriteLine("materia desse dia é: quimica, programação e fisica");
+                    //break;
+
+
+            //case 5:
+                    //Console.WriteLine("materia desse dia é: redes, BDS e matematica");
+                   // break;
+
+                //case 6:
+                    //Console.WriteLine("materia desse dia é: geografia portgues e matematica");
+                    //break;
+
+                //case 7:
+                   // Console.WriteLine("materia desse dia é: historia portgues e biologia");
+                   // break;
+
+               // default:
+                   // Console.WriteLine("opção invalida");
+                        //break;
+
+
+            //}
+            //Console.ReadKey();
+
+
+
+            //case 5:
+
+                //int v=1, k1, mnumero, nnumero, minvalue
+
+            //Console.WriteLine("escreva um numero inteiro");
+            //k1 = int.Parse(Console.ReadLine());
+
+
+            //for (; v != 0;) 
+            //{
+                //mnumero > Minvalue
+                    //mnumero < int.MaxValue;
+            //}
+
+            //if (k1 == 1) ;
+            //Console.WriteLine("escreva um numero inteiro");
+
+            //break;
+
+
+
+            //case 6:
+
+
+                //int r, divisores;
+
+            //
+            //r = int.Parse(Console.ReadLine());
+
+            //if (divisores == r % 2 == 0) 
+               
+
+                 //break;
+
+
+
+
+
+            //case 7:
+                //int g, f, npar, ma;
+            //Console.WriteLine("escreva um numero inteiro");
+            //g = int.Parse(Console.ReadLine());
+
+
+            //for(;g !=0 ; g ++)
+            //{
+              //if (f % 2 == 0 )
+                //{
+                //    ma = npar +  npar
+                //}
+                    
+
+                //else
+                //{
+                    //Console.WriteLine("não foi possivel  calcular a média");
+                //}
+              
+            //}
+               
+           // break;
+
+       
+   // }
+//}
